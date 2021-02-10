70d237b40dfc4ee3dcfac098b36142786e4ea594
Abraham Rivera <a1154451@uabc.edu.mx>
2021-02-10 09:03:31 -0800

    add login POST route

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import Express from 'express';
+import Express, { json } from 'express';
 
 const app = Express();
 const mockUserData = 
@@ -25,5 +25,33 @@ app.get('/users/:id',(req, res) =>
             message: 'Got one user!',
             user: req.params.id
         });
+});
+app.post('/login',Express.json() ,(req, res) =>
+{
+    // Typically passwords are encrypted using something like bcrypt before sending to database
+    const username = req.body.username;
+    const password = req.body.password;
+    // This should come from the database
+    const mockUsername = "billyTheKid";
+    const mockPassword = "superSecret";
+
+    if( username === mockUsername && password === mockPassword)
+    {
+        // In practice, use JSON web token sign method here to make an encrypted token
+        res.json(
+            {
+                success:true,
+                message: 'password and username match!',
+                token: 'Encrypted token goes here'
+            });
+    }
+    else
+    {
+        res.json(
+            {
+                success: false,
+                message: 'password or username do not match'
+            });
+    }
 });
 app.listen(8000, () => console.log("Server is running"));
