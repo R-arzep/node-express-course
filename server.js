2385110219ffecc0f63fa741d2e3387bf1279748
Abraham Rivera <a1154451@uabc.edu.mx>
2021-02-10 08:45:20 -0800

    add first GET route

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 import Express from 'express';
 
 const app = Express();
+const mockUserData = 
+[
+    { name: 'Mark' },
+    { name: 'Jill' }
+];
 
+app.get('/users',(req, res) => 
+{
+    res.json(
+        {
+            success: true,
+            message: 'successfully got users. Nice!',
+            users: mockUserData
+        });
+});
 app.listen(8000, () => console.log("Server is running"));
